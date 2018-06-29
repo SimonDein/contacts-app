84333706ce2802b126dba5c380d06faed4d9e137
Simon Dein <simon.dein@gmail.com>
2018-06-29 22:14:57 +0200

    Added user sign up. Made a couple of changes for better ux.

--- contacts_app.rb
+++ contacts_app.rb
@@ -5,6 +5,7 @@ require 'erubis'
 require 'bcrypt'
 require 'yaml'
 require 'pry'
+require 'fileutils'
 
 require_relative 'lib/contacts.rb'
 
@@ -15,12 +16,14 @@ require_relative 'lib/contacts.rb'
 
 - Sign up
 
-- Profile pictures
-
 - Sort contacts by alphabetical order
   - ascending or descending
 
-FURTHER DEVELOPMENT
+##### FURTHER DEVELOPMENT
+- Profile pictures
+  - Fix src in img for view_contact.erb (already started work)
+  - 
+
 - Search function
     - ajax
 
@@ -59,7 +62,9 @@ def valid_credentials?(user_name, entered_password)
 end
 
 def load_credentials
-  YAML.load_file(credentials_path)
+  credentials = YAML.load_file(credentials_path)
+  return {} if credentials == false
+  credentials
 end
 
 def credentials_path
@@ -77,20 +82,58 @@ def require_user_logged_in
   end
 end
 
-def fetch_profile_picture
-  "/data/no_profile_picture.jpg"
+def user_name_taken?(user_name)
+  credentials = load_credentials
+  credentials.has_key?(user_name)
+end
+
+def user_name_contain_spaces?(user_name)
+  user_name.split.join != user_name
+end
+
+def detect_sign_up_error(user_name, password)
+  case
+  when user_name_taken?(user_name)
+    "Sorry, the username has already been taken."
+  when user_name_contain_spaces?(user_name)
+    "The username can't contain any spaces."
+  when password.strip.length != password.length
+    "The password can't lead or end with a space."
+  end
+end
+
+def encrypt_password(password)
+  BCrypt::Password.create(password)
+end
+
+def create_user(user_name, password)
+  credentials = load_credentials
+  credentials[user_name] = encrypt_password(password)
+
+  File.open('users.yaml', "r+") do |f|
+    f.write(credentials.to_yaml)
+  end
+
+  # Setup user directory and file for contacts
+  FileUtils.mkdir("data/#{user_name}")
+  FileUtils.touch("data/#{user_name}/contacts_db.yaml")
+
 end
 
 #########################################################
 ######################## ROUTES #########################
 #########################################################
 get '/' do
-  redirect '/contacts'
+  redirect '/login'
   # redirect to '/login' if user not logged in
 end
 
 get '/login' do
   erb :login, layout: :layout
+end
+
+get '/signup' do
+  erb :signup, layout: :layout
 end
 
 get '/logout' do
@@ -117,10 +160,16 @@ end
 # View edit contact
 get '/contacts/edit/:id' do
   require_user_logged_in
-
+  
   @id = params[:id]
   @contact = Contacts.new(data_path).get_user(@id)
-  
+
+  @nick_name = @contact.nick_name
+  @full_name = @contact.full_name
+  @email = @contact.email
+  @phone = @contact.phone
+  @address = @contact.address
+
   erb :edit_contact, layout: :layout
 end
 
@@ -151,6 +200,12 @@ end
 
 # Submit added contact
 post '/contacts/add' do
+  @nick_name = params[:nick_name]
+  @full_name = params[:full_name]
+  @email = params[:email]
+  @phone = params[:phone]
+  @address = params[:address]
+  
   if params[:nick_name].empty?
     session[:error] = "You didn't provide a name for the contact."
     erb :add_contact, layout: :layout
@@ -166,31 +221,55 @@ end
 
 # Submit edited contact
 post '/contacts/edit/:id' do
-  id = params[:id]
-  nick_name = params[:nick_name]
+  @nick_name = params[:nick_name]
+  @full_name = params[:full_name]
+  @email = params[:email]
+  @phone = params[:phone]
+  @address = params[:address]
   
-  if nick_name.empty?
+  id = params[:id]
+  
+  if @nick_name.empty?
     session[:error] = "You must provide a nick name"
     erb :edit_contact, layout: :layout
   else
     contacts = Contacts.new(data_path)
     contacts.update_contact(id, params)
     contacts.update!(data_path)
-    session[:success] = "'#{nick_name}' has been updated"
+    session[:success] = "'#{@nick_name}' has been updated"
     redirect '/contacts'
   end
 end
 
 post '/login' do
-  user_name = params[:user_name]
-  password = params[:password]
+  @user_name = params[:user_name].strip
+  @password = params[:password]
 
-  if valid_credentials?(user_name, password)
-    session[:user_name] = user_name
-    session[:success] = "You successfully logged in as '#{user_name}'"
+  if valid_credentials?(@user_name, @password)
+    session[:user_name] = @user_name
+    session[:success] = "You you're now logged in as '#{@user_name}'"
     redirect '/contacts'
   else
     session[:error] = "Sorry, invalid credentials. Please try again."
     erb :login, layout: :layout
   end
 end
+
+post '/signup' do
+  @user_name = params[:user_name]
+  @password = params[:password]
+
+  sign_up_error = detect_sign_up_error(@user_name, @password)
+  if sign_up_error
+    session[:error] = sign_up_error
+    erb :signup, layout: :layout
+  else
+    create_user(@user_name, @password)
+    session[:success] = "The user '#{@user_name}' was created.\nPlease log in."
+    redirect '/login'
+  end
+end
+
+#########################################################
+##################### VIEW HELPERS ######################
+#########################################################
